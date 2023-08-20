a631949176056ad124ca5b4ebcdcabdf1a52f0cc
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-08-20 07:34:38 -0300

    refactor: :recycle: moves data to its own file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,52 +6,7 @@ import {
 } from 'react-icons/fa6'
 import { Link } from '~/components/link'
 import { PostCard } from '~/components/post-card'
-
-const socialMedias = [
-  {
-    name: 'GitHub',
-    Icon: FaGithub,
-    url: 'https://github.com/emiliosheinz',
-  },
-  {
-    name: 'LinkedIn',
-    Icon: FaLinkedin,
-    url: 'https://www.linkedin.com/in/emiliosheinz/',
-  },
-  {
-    name: 'Twitter',
-    Icon: FaTwitter,
-    url: 'https://twitter.com/emiliosheinz',
-  },
-]
-
-const blogPosts = [
-  {
-    title:
-      'Increase React render performance by avoiding unnecessary useEffects',
-    description:
-      'A common mistake I see people making while creating their React component is creating extra states and effects. That may cause unexpected bugs and extra renders.',
-    url: 'https://emiliosheinz.medium.com/increase-react-render-performance-by-avoiding-unnecessary-useeffects-5feea728271b',
-    image:
-      '/images/blog-posts/increase-react-render-performance-by-avoiding-unnecessary-useeffects.webp',
-  },
-  {
-    title: 'How to create a new GitHub Release through Azure Pipelines',
-    description:
-      'Creating a new GitHub release is a common task that a lot of developers are supposed to do during their careers. But on the other hand, it is not as well documented as it could. There are a lot of little tricky things that you will discover only during the process.',
-    url: 'https://medium.com/geekculture/how-to-create-a-new-github-release-through-azure-pipelines-6ee4e61cd7d',
-    image:
-      '/images/blog-posts/how-to-create-a-new-github-release-through-azure-pipelines.webp',
-  },
-  {
-    title: 'How to setup multiple environments in React Native',
-    description:
-      'In this article I’ll explain a bit more about how to setup different environments in React Native and how to properly manage them across Javascript and native code.',
-    url: 'https://medium.com/cwi-software/how-to-setup-multiple-environments-for-react-native-786005a360fc',
-    image:
-      '/images/blog-posts/how-to-setup-multiple-environments-in-react-native.webp',
-  },
-]
+import { blogPosts, socialMedias } from '~/utils/data.utils'
 
 export default function Home() {
   return (
