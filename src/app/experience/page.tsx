9f512bbef78016032ecccf92868b6b8ca4dfd9b4
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-08-22 18:56:58 -0300

    feat: :sparkles: addin all my experiences

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -4,6 +4,7 @@ import { experiences } from '~/content/experiences'
 export default function Experience() {
   return (
     <div className='flex flex-col gap-8 mt-20'>
+      <h1 className='font-bold text-5xl mb-5'>Experience</h1>
       {experiences.map(experience => (
         <ExperienceCard
           key={experience.title}
