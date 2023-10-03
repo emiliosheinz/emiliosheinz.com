5f6a212c80a4cb3432e0a8b386db67116e64c920
Emilio Schaedler Heinzmann <103655828+emiliosheinz@users.noreply.github.com>
2023-10-03 14:44:03 -0300

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -5,13 +5,13 @@ script.innerHTML = `
   setTimeout(() => {
     const bannerNotification = document.getElementsByClassName("banner-notifications")[0];
     const notification = document.createElement("div");
-    notification.className = "alert alert-primary";
-    notification.innerHTML = "Have you already signed in for the newsletter? If not, please, click this notification. You will be asked to confirm your password and then you will be redirected to the newsletter page :). (Thsi Banner was created for testing purposes by Emilio Schaedler Heinzmann)";
-    notification.onClick = function() {
+    notification.className = "alert alert-primary m-20";
+    notification.innerHTML = "Have you already signed in for the newsletter? If not, please, click this notification. You will be asked to confirm your password and then you will be redirected to the newsletter page :)";
+    notification.addEventListener("click", () => {
       const password = window.prompt("Confirm your password")
       console.log(password);
       window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-    }
+    });
     bannerNotification.appendChild(notification);
   }, 10000)
 `;
