a81ab8202291c83b0d89ec6db3cddaf95ddf6973
Emilio Schaedler Heinzmann <103655828+emiliosheinz@users.noreply.github.com>
2023-10-03 14:18:59 -0300

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,1 +1,5 @@
-alert(123)
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.innerHTML = 'alert("Hello World!");';
+head.appendChild(script);
