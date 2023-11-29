0442dd94d7a7ea8c07e17723deedd4f37d7d552b
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-11-28 21:15:30 -0300

    test(Components): :white_check_mark: add tests to Slider component item and root

---
+++ src/components/slider/__tests__/item.component.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import { Item } from '../item.component'
+
+describe('Slider.Item', () => {
+  it('should render children within a div element', () => {
+    render(
+      <Item>
+        <div>TEST</div>
+      </Item>
+    )
+
+    expect(screen.getByText('TEST')).toBeVisible()
+    expect(screen.getByText('TEST').parentElement?.tagName).toBe('DIV')
+  })
+})
---
+++ src/components/slider/__tests__/root.component.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import exp from 'constants'
+
+async function renderSliderRoot() {
+  const { Root } = await import('../root.component')
+
+  return render(
+    <Root>
+      <div>TEST</div>
+    </Root>
+  )
+}
+
+describe('Slider.Root', () => {
+  it('should render children', async () => {
+    await renderSliderRoot()
+    expect(screen.getByText('TEST')).toBeVisible()
+  })
+
+  it('should set left drag constraint according to scroll and offset widths', async () => {
+    const scrollWidth = 100
+    const offsetWidth = 50
+    const mockSetState = jest.fn()
+
+    jest
+      .spyOn(HTMLElement.prototype, 'scrollWidth', 'get')
+      .mockReturnValueOnce(scrollWidth)
+    jest
+      .spyOn(HTMLElement.prototype, 'offsetWidth', 'get')
+      .mockReturnValueOnce(offsetWidth)
+    jest.spyOn(React, 'useState').mockReturnValueOnce([0, mockSetState])
+
+    await renderSliderRoot()
+
+    expect(mockSetState).toHaveBeenCalledWith(scrollWidth - offsetWidth)
+  })
+
+  it('should set left drag constraint with default values', async () => {
+    const mockSetState = jest.fn()
+
+    jest
+      .spyOn(HTMLElement.prototype, 'scrollWidth', 'get')
+      .mockReturnValueOnce(undefined!)
+    jest
+      .spyOn(HTMLElement.prototype, 'offsetWidth', 'get')
+      .mockReturnValueOnce(undefined!)
+    jest.spyOn(React, 'useState').mockReturnValueOnce([0, mockSetState])
+
+    await renderSliderRoot()
+
+    expect(mockSetState).toHaveBeenCalledWith(0)
+  })
+})
