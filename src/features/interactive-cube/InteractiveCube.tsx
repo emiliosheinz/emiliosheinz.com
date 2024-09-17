4f859c3e816052c9975054d7d0dd9d3afcf8fdb5
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-09-16 21:05:23 -0300

    feat: trying overlay with radix ui

--- src/features/interactive-cube/InteractiveCube.tsx
+++ src/features/interactive-cube/InteractiveCube.tsx
@@ -3,30 +3,46 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { PointLight } from "three";
+import * as Dialog from "@radix-ui/react-dialog";
 import { Cube } from "./Cube";
 import { useIsSpacePressed } from "./useIsSpacePressed";
+import { useState } from "react";
 
 export function InteractiveCube() {
+  const [isFocused, setIsFocused] = useState(false);
   const isSpacePressed = useIsSpacePressed() || true;
 
-  return (
-    <>
-      <Canvas
-        camera={{ position: [6, 6, 6], fov: 30 }}
-        onCreated={({ camera, scene }) => {
-          const light = new PointLight();
-          light.position.set(0, 1, 0);
-          light.intensity = 300;
+  console.log(isFocused);
 
-          camera.add(light);
-          scene.add(camera);
-        }}
-        style={{ width: "100%", height: "100%" }}
-      >
-        <Cube />
-        {isSpacePressed && <OrbitControls autoRotate enableZoom={false} />}
-        <ambientLight intensity={0.3} />
-      </Canvas>
-    </>
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Canvas
+          camera={{ position: [6, 6, 6], fov: 30 }}
+          onCreated={({ camera, scene }) => {
+            const light = new PointLight();
+            light.position.set(0, 1, 0);
+            light.intensity = 300;
+
+            camera.add(light);
+            scene.add(camera);
+          }}
+        >
+          <Cube />
+          {isSpacePressed && <OrbitControls autoRotate enableZoom={false} />}
+          <ambientLight intensity={0.3} />
+        </Canvas>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+        <Dialog.Content>
+          <Dialog.Title>Interactive Cube</Dialog.Title>
+          <Dialog.Description>
+            Click and drag to rotate the cube. Press space to auto-rotate.
+          </Dialog.Description>
+          <Dialog.Close />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
